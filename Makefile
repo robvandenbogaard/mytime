df0626397d734c3b7232268f2101675a87f0eb94
Rob van den Bogaard <robvandenbogaard@gmail.com>
2024-05-19 13:57:15 +0200

    Make starting, stopping and logging work, and add make target serve for running elm reactor

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 build:
 	elm make src/Main.elm --debug --output index.html
 
+serve:
+	elm reactor --port=8011
--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,9 @@ module Main exposing (main)
 
 import Browser
 import Dict exposing (Dict)
-import Html
+import Html exposing (Html)
 import Html.Attributes as Html
+import Html.Events as Html
 import Task
 import Time
 
@@ -22,37 +23,56 @@ init flags =
     ( { log = logOfTheDayExample
       , activity = Nothing
       , time = Nothing
+      , timezone = Nothing
       }
-    , Task.perform GotTime Time.now
+    , Task.perform GotTimeZone Time.here
     )
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotTime time ->
-            ( { model | time = Just time }, Cmd.none )
+        GotTimeAfter runningActivity time ->
+            let
+                log =
+                    logActivity runningActivity time model.time model.log
+            in
+            ( { model | time = Just time, log = log }, Cmd.none )
+
+        GotTimeZone zone ->
+            ( { model | timezone = Just zone }, Cmd.none )
 
         Start activity ->
-            -- TODO: Save running activity to log
-            ( { model
-                | activity =
-                    case model.time of
-                        Nothing ->
-                            Nothing
-
-                        Just time ->
-                            Just { name = activity, startedAt = time }
-              }
-            , Cmd.none
-            )
+            ( { model | activity = Just activity }, Task.perform (GotTimeAfter model.activity) Time.now )
 
         Stop ->
-            -- TODO: Save running activity to log
-            ( { model | activity = Nothing }, Cmd.none )
+            ( { model | activity = Nothing }, Task.perform (GotTimeAfter model.activity) Time.now )
 
 
+logActivity : Maybe ActivityName -> Time.Posix -> Maybe Time.Posix -> LogOfTheDay -> LogOfTheDay
+logActivity activity stoppedAt maybeStartedAt log =
+    case maybeStartedAt of
+        Nothing ->
+            log
 
---    ( model, Cmd.none )
+        Just startedAt ->
+            case activity of
+                Nothing ->
+                    log
+
+                Just name ->
+                    log
+                        |> Dict.update name
+                            (\entries ->
+                                entries
+                                    |> Maybe.withDefault Dict.empty
+                                    |> Dict.insert (Time.posixToMillis startedAt) (toFloat (Time.posixToMillis stoppedAt - Time.posixToMillis startedAt) / oneHourInMillis)
+                                    |> Just
+                            )
+
+
+oneHourInMillis =
+    3600000
 
 
 subscriptions model =
@@ -61,21 +81,17 @@ subscriptions model =
 
 type alias Model =
     { log : LogOfTheDay
-    , activity : Maybe Activity
+    , activity : Maybe ActivityName
     , time : Maybe Time.Posix
+    , timezone : Maybe Time.Zone
     }
 
 
 type Msg
-    = GotTime Time.Posix
+    = GotTimeAfter (Maybe ActivityName) Time.Posix
+    | GotTimeZone Time.Zone
     | Start ActivityName
     | Stop
-
-
-type alias Activity =
-    { name : ActivityName
-    , startedAt : Time.Posix
-    }
 
 
 type alias Log =
@@ -99,46 +115,56 @@ type alias LogEntries =
 
 
 type alias LogTime =
-    String
+    -- millis from posix time
+    Int
 
 
 type alias Hours =
     Float
 
 
+view : Model -> Browser.Document Msg
 view model =
     { title = "My time"
     , body =
         [ Html.main_ [ Html.style "margin" "1em" ]
-            [ viewLogByTime model.log
-            , viewLog model.log
-            , viewStartableActivities model.log
-            , Html.section [ Html.style "margin" "1em" ]
-                [ Html.input
-                    [ Html.style "width" "100%"
-                    , Html.style "max-width" "20em"
-                    , Html.placeholder "add other activity"
+            (case model.timezone of
+                Nothing ->
+                    [ Html.text "timezone not yet known" ]
+
+                Just timezone ->
+                    [ viewLogByTime timezone model.log
+                    , viewLog timezone model.log
+                    , viewStartableActivities model.activity model.log
+                    , Html.section [ Html.style "margin" "1em" ]
+                        [ Html.input
+                            [ Html.style "width" "100%"
+                            , Html.style "max-width" "20em"
+                            , Html.placeholder "add other activity"
+                            ]
+                            []
+                        ]
+                    , Html.section [ Html.style "margin" "1em" ]
+                        [ Html.button [ Html.onClick Stop ] [ Html.text "Stop" ] ]
                     ]
-                    []
-                ]
-            , Html.section [ Html.style "margin" "1em" ]
-                [ Html.button [] [ Html.text "Stop" ] ]
-            ]
+            )
         ]
     }
 
 
+logOfTheDayExample : LogOfTheDay
 logOfTheDayExample =
-    [ ( "lunch", Dict.fromList [ ( "12:00", 1 ) ] )
+    [ ( "lunch", Dict.empty )
     , ( "break", Dict.empty )
     , ( "mail", Dict.empty )
     , ( "sanitary", Dict.empty )
-    , ( "write report", Dict.fromList [ ( "10:00", 2 ), ( "13:00", 1 ) ] )
+    , ( "write report", Dict.empty )
     ]
         |> Dict.fromList
 
 
-viewLogByTime activities =
+viewLogByTime : Time.Zone -> LogOfTheDay -> Html Msg
+viewLogByTime timezone activities =
     activities
         |> Dict.toList
         |> List.map
@@ -149,49 +175,66 @@ viewLogByTime activities =
             )
         |> List.concat
         |> List.sortBy (\( a, b, c ) -> a)
-        |> List.map viewTimeEntry
+        |> List.map (viewTimeEntry timezone)
         |> Html.section [ Html.style "margin" "1em" ]
 
 
-viewTimeEntry ( time, activity, hours ) =
-    Html.article [] [ Html.text <| time ++ " - " ++ String.fromFloat hours ++ "h " ++ activity ]
+viewTimeEntry timezone ( time, activity, hours ) =
+    Html.article [] [ Html.text <| viewTime timezone time ++ " - " ++ String.fromFloat hours ++ "h " ++ activity ]
 
 
-viewLog activities =
+viewLog : Time.Zone -> LogOfTheDay -> Html Msg
+viewLog timezone activities =
     activities
         |> Dict.toList
-        |> List.map viewLogEntriesForActivity
+        |> List.map (viewLogEntriesForActivity timezone)
         |> Html.section [ Html.style "margin" "1em" ]
 
 
-viewLogEntriesForActivity ( activity, entries ) =
+viewLogEntriesForActivity timezone ( activity, entries ) =
     entries
         |> Dict.toList
-        |> List.map viewLogEntries
+        |> List.map (viewLogEntries timezone)
         |> Html.dd []
         |> List.singleton
         |> (::) (Html.dt [] [ Html.text activity ])
         |> Html.dl []
 
 
-viewLogEntries ( time, hours ) =
+viewLogEntries timezone ( time, hours ) =
     Html.dd []
-        [ Html.text (time ++ " - " ++ String.fromFloat hours ++ " uur") ]
+        [ Html.text (viewTime timezone time ++ " - " ++ String.fromFloat hours ++ " uur") ]
 
 
-viewStartableActivities activities =
+viewTime zone time =
+    let
+        posix =
+            Time.millisToPosix time
+    in
+    (Time.toHour zone posix
+        |> String.fromInt
+        |> String.pad 2 '0'
+    )
+        ++ ":"
+        ++ (Time.toMinute zone posix
+                |> String.fromInt
+                |> String.pad 2 '0'
+           )
+
+
+viewStartableActivities runningActivity activities =
     activities
         |> Dict.keys
-        |> List.map viewStartableActivity
+        |> List.map (viewStartableActivity runningActivity)
         |> Html.section [ Html.style "margin" "1em" ]
 
 
-viewStartableActivity activity =
+viewStartableActivity runningActivity activity =
     Html.article
         [ Html.style "display" "flex"
         , Html.style "width" "100%"
         , Html.style "max-width" "20em"
         ]
         [ Html.span [ Html.style "flex" "1" ] [ Html.text activity ]
-        , Html.button [] [ Html.text "Start" ]
+        , Html.button [ Html.disabled (runningActivity == Just activity), Html.onClick (Start activity) ] [ Html.text "Start" ]
         ]
